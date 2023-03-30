0fb61306c1e449d4ce277ee088b1f6fdfa1f2d2d
Rubs <113114471+rvbs1337@users.noreply.github.com>
2023-03-30 15:09:47 -0300

    Update DESAFIOPROFISSIONAL.cpp

--- DESAFIOPROFISSIONAL.cpp
+++ DESAFIOPROFISSIONAL.cpp
@@ -4,28 +4,24 @@
 #include <string.h>
 #include <windows.h>
 
-/*CADASTRO VERSÃO 4.0
+
+/*CADASTRO VERSÃO 5.0
 1.0 - Adicionado cadastro da placa;
-
 1.1 - registro da placa agora é feito por um procedimento;
-
 1.2 - Adicionado erro pro cadastro da placa;
-
 2.0 - Adicionar seleção de marca;
-
 2.1 - Refatorado seleção de marca;
-
 3.0 - Seleção de Veiculo;
-
-4.0 - Registro de chassi;
+4.0 - Seleção de Setor;
+5.0 - Registro de Chassi;
 */
 
 char placa[8];
 char chassi[18];
 char aux[2];
-int placaVet[7],chassiVet[17], marcaN, modeloN;
+int placaVet[7],chassiVet[17], marcaN, modeloN, setorN;
 bool valido = true;
-char marca[10][15] = {"CHEVROLET","CITROEN","FIAT","HONDA","JEEP","KIA","PEUGEOT","RENAULT","TOYOTA","VOLKSWAGEN"}, marcaVeiculo[15], modeloVeiculo[15];
+char marca[10][15] = {"CHEVROLET","CITROEN","FIAT","HONDA","JEEP","KIA","PEUGEOT","RENAULT","TOYOTA","VOLKSWAGEN"}, marcaVeiculo[15], modeloVeiculo[15],setorVeiculo[15];
 char modeloGM[14][15] = {"S10","D-20","CLASSIC","MONTANA","SPIN","COBALT","ONIX","CRUZE","CORSA","OMEGA","ASTRA","PRISMA","BAZER","ZAFIRA"};
 char modeloCIT[3][15] = {"JUMPER","JUMPY","AIRCROSS"};
 char modeloFiat[7][15] = {"PALIO","STRADA","UNO","DOBLO","CARGO","MULTIJET","FIORINO"};
@@ -36,7 +32,46 @@ char modeloPG[3][15] = {"PARTNER","BOXER","207"};
 char modeloToyo[3][15] = {"HILUX","BANDEIRANTES","HILUX SW4"};
 char modeloRN[5][15] = {"MASTER","KANGOO","LOGAN","DUSTER","KWID"};
 char modeloVW[5][15] = {"GOL","KOMBI","PARATI","SAVEIRO","VOYAGE"};
+char setor[10][30] = {"SETRAB","SELURB","SEINFRA","SEMOP","FUNREBOM","SECRETARIA DE SAUDE","SEMUSP ADMINISTRAÇÃO FLORIANO","SEMUSP ADMINISTRAÇÃO IGUATEMI", "AMITECH", "SEMOB"};
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+void loading();
+void registrarPlaca();
+void registrarMarca();
+void registrarChassi();
+void registrarSetor();
+//------------------------------------------------------------------------------
 
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	do{
+		registrarPlaca();
+		if(!valido){
+			printf("Erro, um ou mais caracteres invalidos.\nO campo deve ser preenchido com o padrão ABC1234 ou ABC1D23.\n");
+			printf("Por favor preencha novamente.\n");
+		}
+	}while(!valido);
+	
+	loading();
+	
+	registrarMarca();
+	
+	loading();
+	
+	registrarChassi();
+	
+	loading();
+	
+	printf("Placa: %s \n", placa);
+	printf("Marca: %s \n", marcaVeiculo);
+	printf("Modelo: %s \n", modeloVeiculo);
+	printf("Chassi: %s \n", chassi);
+	printf("Setor: %s \n", setorVeiculo);
+	
+    system("pause");
+}
+
+//------------------------------------------------------------------------------
 void loading(){
 	system("cls");
 	for(int i = 0; i<2; i++){
@@ -51,7 +86,7 @@ void loading(){
 		system("cls");
 	}
 }
-
+//------------------------------------------------------------------------------
 void registrarPlaca(){
 		valido = true;
 		
@@ -111,7 +146,7 @@ void registrarPlaca(){
         }            
 
 }
-
+//------------------------------------------------------------------------------
 void registrarMarca(){
 	
 	printf("O veículo é de que marca? Digite o número:\n");
@@ -122,115 +157,154 @@ void registrarMarca(){
     
 	scanf("%i", &marcaN);
 	fflush(stdin);
-    
     system("cls");
     
     switch(marcaN){
 
 			case 1:
 				strcpy(marcaVeiculo, marca[0]);
-				for(int i = 0; i < 14; i++){
+				for(int i = 0; i < 14; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloGM[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
 						strcpy(modeloVeiculo,modeloGM[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
 			break;
 			case 2:
 				strcpy(marcaVeiculo, marca[1]);	
-				for(int i = 0; i < 3; i++){
+				for(int i = 0; i < 3; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloCIT[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloCIT[modeloN-1]);	
+						strcpy(modeloVeiculo,modeloCIT[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
 			break;
 			case 3:
 				strcpy(marcaVeiculo, marca[2]);	
-				for(int i = 0; i < 7; i++){
+				for(int i = 0; i < 7; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloFiat[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloFiat[modeloN-1]);	
+						strcpy(modeloVeiculo,modeloFiat[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
 			break;
             case 4:
 				strcpy(marcaVeiculo, marca[3]);		
-				for(int i = 0; i < 2; i++){
+				for(int i = 0; i < 2; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloHD[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloHD[modeloN-1]);  
+						strcpy(modeloVeiculo,modeloHD[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;
             case 5:
                 strcpy(marcaVeiculo, marca[4]);	
-				for(int i = 0; i < 1; i++){
+				for(int i = 0; i < 1; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloJeep[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloJeep[modeloN-1]);   
+						strcpy(modeloVeiculo,modeloJeep[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;   
             case 6:
                 strcpy(marcaVeiculo, marca[5]);
-				for(int i = 0; i < 1; i++){
+				for(int i = 0; i < 1; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloKia[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloKia[modeloN-1]);	   
+						strcpy(modeloVeiculo,modeloKia[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;   
             case 7:
                 strcpy(marcaVeiculo, marca[6]);	 
-				for(int i = 0; i < 3; i++){
+				for(int i = 0; i < 3; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloPG[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloPG[modeloN-1]);  
+						strcpy(modeloVeiculo,modeloPG[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;    
             case 8:
                 strcpy(marcaVeiculo, marca[7]);	
-				for(int i = 0; i < 5; i++){
+				for(int i = 0; i < 5; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloRN[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloRN[modeloN-1]); 
+						strcpy(modeloVeiculo,modeloRN[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;   
             case 9:
                 strcpy(marcaVeiculo, marca[8]);	
-				for(int i = 0; i < 3; i++){
+				for(int i = 0; i < 3; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloToyo[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloToyo[modeloN-1]);    
+						strcpy(modeloVeiculo,modeloToyo[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;   
             case 10:
                 strcpy(marcaVeiculo, marca[9]);	
-				for(int i = 0; i < 5; i++){
+				for(int i = 0; i < 5; i++){ //seleção dos modelos
 						printf("%d - %s\n", i+1, modeloVW[i]);	
 						}
 						scanf("%d", &modeloN);
 						fflush(stdin);
 						
-						strcpy(modeloVeiculo,modeloVW[modeloN-1]);   
+						strcpy(modeloVeiculo,modeloVW[modeloN-1]);
+						
+						system("cls");
+						
+						registrarSetor();
             break;   
                 
        }
 }
-
+//------------------------------------------------------------------------------
 void registrarChassi(){
 		valido = true;
 		
@@ -340,34 +414,13 @@ void registrarChassi(){
 
 	}
 }
-
-int main(){
-	setlocale(LC_ALL, "portuguese");
-	
-	do{
-		registrarPlaca();
-		if(!valido){
-			printf("Erro, um ou mais caracteres invalidos.\nO campo deve ser preenchido com o padrão ABC1234 ou ABC1D23.\n");
-			printf("Por favor preencha novamente.\n");
-		}
-	}while(!valido);
-	
-	loading();
-	
-	registrarMarca();
-	
-	loading();
-	
-	registrarChassi();
-	
-	loading();
-	
-	printf("Placa: %s \n", placa);
-	printf("Marca: %s \n", marcaVeiculo);
-	printf("Modelo: %s \n", modeloVeiculo);
-	printf("Chassi: %s \n", chassi);
-	
-    system("pause");
-
-	
+//-------------------------------------------------------------------------------
+void registrarSetor(){
+	for(int i = 0; i < 10; i++){ //seleção do setor
+    printf("%d - %s\n", i+1, setor[i]);	
+   	}
+   	scanf("%d", &setorN);
+	fflush(stdin);
+						
+	strcpy(setorVeiculo,setor[setorN-1]);
 }
